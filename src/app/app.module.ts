e72e463674727d66bc514634bc208a1540e16059
adkonte <alykonte19@gmail.com>
2023-02-03 20:42:30 +0000

    commit du vendredi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -41,6 +42,7 @@ import { UtilisateursComponent } from './utilisateurs/utilisateurs.component';
     ReactiveFormsModule,
     Ng2SearchPipeModule,
     
+
 
   ],
   providers: [],
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,45 +7,37 @@ import { Boutiques } from '../Model/boutiques';
 import { User } from '../Model/user';
 import { UserService } from '../Services/authentification/utilisateur.service';
 import { BoutiquesService } from '../Services/boutiques.service';
-// import Chart from 'chart.js/auto';
-
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  jaimes: any;
+  afficherBoutique: any;
+  // form: FormGroup;
 
+  boutique: Boutiques = {
+    nom: '',
+    description: '',
+    adresse: '',
+    etat: false,
+    user_id: new User(),
+  };
 
-    jaimes: any
-    afficherBoutique: any;
-   // form: FormGroup;
+  isLoggedIn = false;
+  isLoginFailed = false;
+  message?: string;
 
+  errorMessage = '';
+  content?: string;
+  contenu?: string;
+  etat: any;
 
-
-   
-    boutique: Boutiques = {
-        nom: '',
-        description: '',
-        adresse: '',
-        etat: false,
-        user_id: new User
-      };
-
-    isLoggedIn = false;
-    isLoginFailed = false;
-    message?: string;
-
-    errorMessage = '';
-    content?: string;
-    contenu?: string;
-    etat:any
-
-
-
-
-public chart : any;
+  public chart: any;
+  public Piecharts: any;
   basicData: any;
 
   multiAxisData: any;
@@ -57,288 +49,155 @@ public chart : any;
   basicOptions: any;
   formmodule!: FormGroup;
 
-  
-    file: any;
+  aly=12;
+   mali=13
+  file: any;
   user: any;
-  constructor(private fb: FormBuilder,
-    private boutiqueservice : BoutiquesService,
+  constructor(
+    private fb: FormBuilder,
+    private boutiqueservice: BoutiquesService,
     private route: ActivatedRoute,
     private routes: Router,
-    private userService: UserService) {
- 
-        
+    private userService: UserService
+  ) {}
+
+  //chart line
+  createChart(){
+  
+    this.chart = new Chart("MyChart", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['jan', 'fe', 'mars','mai',
+								 'juin', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: [],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: [this.aly,this.mali,this.aly,0,10,17],
+            // backgroundColor: 'limegreen',
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+  }
+//chart pie
+PieChart(){
+  
+  this.Piecharts = new Chart("pieChart", {
+    type: 'pie', //this denotes tha type of chart
+
+    data: {// values on X-Axis
+      labels: ['jan', 'fe', 'mars','mai',
+               'juin', '2022-05-15', '2022-05-16','2022-05-17', ], 
+       datasets: [
+        // {
+        //   label: "Sales",
+        //   data: [],
+        //   backgroundColor: 'blue'
+        // },
+        {
+          label: "Profit",
+          data: [this.aly,this.mali,this.aly,0,10,17],
+          // backgroundColor: 'limegreen',
+        }  
+      ]
+    },
+    options: {
+      aspectRatio:2.5
+    }
+    
+  });
 }
-
-
   ngOnInit(): void {
-
-
-    //chart
-    // this.createChart();
-
-
- 
+  
     //
     // this.boutiqueservice.ajouterBout(this.boutique.nom, this.boutique.adresse, this.boutique.description, this.boutique.etat, this.boutique.user_id, this.file).subscribe(data=>{
-        
+
     //     this.boutiques =data
     // })
+    this.createChart();
 
 
-    this.boutiqueservice.getAll().subscribe(data=>{
-        this.afficherBoutique = data
-        console.log("Afficher la  " +this.afficherBoutique);
+    this.boutiqueservice.getAll().subscribe((data) => {
+      this.afficherBoutique = data;
+      console.log('Afficher la  ' + this.afficherBoutique);
+    });
+    //liste des utilisateurs
 
-      })
-      //liste des utilisateurs
+    this.userService.getAllUser().subscribe((data) => {
+      this.user = data;
+    });
 
-      this.userService.getAllUser().subscribe(data=>{
-        this.user = data
-      })
-
-    
     this.formmodule = this.fb.group({
-        nom: ["", Validators.required],
-        file: ["", Validators.required],
-        description: ["", Validators.required],
-        adresse: ["", Validators.required],
-        etat:["",Validators.required],
-        user_id:["",Validators.required]
-      })
+      nom: ['', Validators.required],
+      file: ['', Validators.required],
+      description: ['', Validators.required],
+      adresse: ['', Validators.required],
+      etat: ['', Validators.required],
+      user_id: ['', Validators.required],
+    });
 
-//     this.multiAxisData = {
-//       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//       datasets: [{
-//           label: 'Dataset 1',
-//           fill: false,
-//           borderColor: '#42A5F5',
-//           yAxisID: 'y',
-//           tension: .4,
-//           data: [65, 59, 80, 81, 56, 55, 10]
-//       }, {
-//           label: 'Dataset 2',
-//           fill: false,
-//           borderColor: '#00bb7e',
-//           yAxisID: 'y1',
-//           tension: .4,
-//           data: [28, 48, 40, 19, 86, 27, 90]
-//       }]
-//   };
-
-//   this.multiAxisOptions = {
-//       stacked: false,
-//       plugins: {
-//           legend: {
-//               labels: {
-//                   color: '#495057'
-//               }
-//           }
-//       },
-//       scales: {
-//           x: {
-//               ticks: {
-//                   color: '#495057'
-//               },
-//               grid: {
-//                   color: '#ebedef'
-//               }
-//           },
-//           y: {
-//               type: 'linear',
-//               display: true,
-//               position: 'left',
-//               ticks: {
-//                   color: '#495057'
-//               },
-//               grid: {
-//                   color: '#ebedef'
-//               }
-//           },
-//           y1: {
-//               type: 'linear',
-//               display: true,
-//               position: 'right',
-//               ticks: {
-//                   color: '#495057'
-//               },
-//               grid: {
-//                   drawOnChartArea: false,
-//                   color: '#ebedef'
-//               }
-//           }
-//       }
-//   };
-
-//   this.multiAxisOptions = {
-//     stacked: false,
-//     plugins: {
-//         legend: {
-//             labels: {
-//                 color: '#495057'
-//             }
-//         }
-//     },
-//     scales: {
-//         x: {
-//             ticks: {
-//                 color: '#495057'
-//             },
-//             grid: {
-//                 color: '#ebedef'
-//             }
-//         },
-//         y: {
-//             type: 'linear',
-//             display: true,
-//             position: 'left',
-//             ticks: {
-//                 color: '#495057'
-//             },
-//             grid: {
-//                 color: '#ebedef'
-//             }
-//         },
-//         y1: {
-//             type: 'linear',
-//             display: true,
-//             position: 'right',
-//             ticks: {
-//                 color: '#495057'
-//             },
-//             grid: {
-//                 drawOnChartArea: false,
-//                 color: '#ebedef'
-//             }
-//         }
-//     }
-// };
-// this.multiAxisOptions = {
-//   stacked: false,
-//   plugins: {
-//       legend: {
-//           labels: {
-//               color: '#ebedef'
-//           }
-//       }
-//   },
-//   scales: {
-//       x: {
-//           ticks: {
-//               color: '#ebedef'
-//           },
-//           grid: {
-//               color: 'rgba(255,255,255,0.2)'
-//           }
-//       },
-//       y: {
-//           type: 'linear',
-//           display: true,
-//           position: 'left',
-//           ticks: {
-//               color: '#ebedef'
-//           },
-//           grid: {
-//               color: 'rgba(255,255,255,0.2)'
-//           }
-//       },
-//       y1: {
-//           type: 'linear',
-//           display: true,
-//           position: 'right',
-//           ticks: {
-//               color: '#ebedef'
-//           },
-//           grid: {
-//               drawOnChartArea: false,
-//               color: 'rgba(255,255,255,0.2)'
-//           }
-//       }
-//   }
-// };
-}
-// onSubmit() {
-//     if (this.form.valid) {
-//       // Traitez les données du formulaire ici
-//     }
-//   }
-
-
-//chart
-// createChart() {
-//     this.chart = new Chart("MyChart", {
-//       type: 'bar',
-//       data: {
-//         labels: ['Jan', 'Fev', 'Mars', 'Av', 'Mai', 'Juin', 'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Dec'],
-//         datasets: [
-//           {
-//             label: "Sales",
-//             data: [467, 577, 572, 79, 92, 574, 573, 576, 79, 92, 574, 573],
-//             backgroundColor: '#695CFE'
-//           },
-//           {
-//             label: "Profit",
-//             data: [542, 542, 536, 327, 17, 0.00, 538, 541, 79, 92, 574, 1000],
-//             backgroundColor: 'limegreen'
-//           }
-//         ]
-//       },
-//       options: {
-//         aspectRatio: 2.5
-//       }
-//     });
-
-//   }
-
-fileChang(event: any) {
-    this.file = event.target.files[0];
-    console.log(this.file)
   }
 
 
-//la methode de jaime
+  fileChang(event: any) {
+    this.file = event.target.files[0];
+    console.log(this.file);
+  }
 
-Jaime(){
+  //la methode de jaime
+
+  Jaime() {
     // this.boutiqueservice.Jaime(this.jaimes, this.)
-}
+  }
 
-
-ajoutBout(){
-    let data = new FormData()
-    if(this.etat === "0"){
-        this.boutique.etat = false
-    }else{
-        this.boutique.etat = true
+  ajoutBout() {
+    let data = new FormData();
+    if (this.etat === '0') {
+      this.boutique.etat = false;
+    } else {
+      this.boutique.etat = true;
     }
-console.log(this.boutique)
-    this.boutiqueservice.ajouterBoutique(this.boutique.nom, this.boutique.adresse, this.boutique.description, this.boutique.etat, this.boutique.user_id, this.file).subscribe(data=>{
-        this.boutique = this.formmodule.value
-       this.afficherBoutique =data
-       Swal.fire({
-        heightAuto: false,
-        // position: 'top-end',
-        icon: 'success',
-        text: 'Boutique créée avec succès',
-        showConfirmButton: false,
-        timer: 2500
-      })
-     // this.routes.navigateByUrl("/sidebar/dashboard")
-   // La methode permettra d'actualiser la page apres l'ajout
-     window.location.reload();
+    console.log(this.boutique);
+    this.boutiqueservice
+      .ajouterBoutique(
+        this.boutique.nom,
+        this.boutique.adresse,
+        this.boutique.description,
+        this.boutique.etat,
+        this.boutique.user_id,
+        this.file
+      )
+      .subscribe((data) => {
+        this.boutique = this.formmodule.value;
+        this.afficherBoutique = data;
+        Swal.fire({
+          heightAuto: false,
+          // position: 'top-end',
+          icon: 'success',
+          text: 'Boutique créée avec succès',
+          showConfirmButton: false,
+          timer: 2500,
+        });
+        // this.routes.navigateByUrl("/sidebar/dashboard")
+        // La methode permettra d'actualiser la page apres l'ajout
+        window.location.reload();
 
-     //finnnnnnnnnnnnnnnnnnnnnnnnnnn
-        this.formmodule.reset()
-         this.message = " Boutique ajouté avec succès";
-         this.contenu = data.contenu
-        
-    })
+        //finnnnnnnnnnnnnnnnnnnnnnnnnnn
+        this.formmodule.reset();
+        this.message = ' Boutique ajouté avec succès';
+        this.contenu = data.contenu;
+      });
+  }
+
+  //listes de boutiques
 }
-
-
-//listes de boutiques
-
-
-}
-
-
-
-  
